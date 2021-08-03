2424fb63287dd001b243a486ea0d5c97aed02b2c
ymkim97 <rladudaud97@naver.com>
2021-08-04 00:58:42 +0900

    1406번 문제 해결

--- 1406.py
+++ 1406.py
@@ -1,3 +1,35 @@
+#시간 해결
+
+import sys
+
+Leftstack = list(sys.stdin.readline().rstrip())
+
+command_number = int(sys.stdin.readline().rstrip())
+
+Rightstack = []
+
+for _ in range(command_number):
+    
+    string = sys.stdin.readline().rstrip().split()
+    
+    if string[0] == 'L' and len(Leftstack) != 0:
+        Rightstack.append(Leftstack.pop())
+
+    elif string[0] == 'D' and len(Rightstack) != 0:
+        Leftstack.append(Rightstack.pop())
+
+    elif string[0] == 'P':
+        Leftstack.append(string[1])
+
+    elif string[0] == 'B' and len(Leftstack) != 0:
+        Leftstack.pop()
+
+while len(Rightstack) != 0:
+    Leftstack.append(Rightstack.pop())        
+
+print("".join(Leftstack))
+
+""" 시간 Over 2ms
 import sys
 
 string = list(sys.stdin.readline().rstrip())
@@ -52,3 +84,5 @@ for _ in range(command_number):
         cursor -= 1
 
 print("".join(string))
+
+"""
