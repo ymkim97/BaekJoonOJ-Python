da6758bfee60c6aaa0a187f584cb01d409fd5131
ymkim97 <rladudaud97@naver.com>
2021-11-18 01:20:57 +0900

    code2

---
+++ code2.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+a = []
+
+while True:
+    n = int(input())
+    
+    if n != 0:
+        a.append(n)
+
+    else:
+
+        index = int(input())
+        num = int(input())
+        if index < 0:
+            index = len(a) + index
+
+        if index >= len(a):
+            print("Not in range")
+            break
+
+        else:
+            for i in range(0, index + 1):
+                if a[i] > num:
+                    print(a[i])            
+            break
